72d2507b90caa18c2cca55f1acfdc8ef79eecd09
Patrik Jonsson <patrik.jonsson@gmail.com>
2023-12-09 10:46:05 +0100

    Day 9

---
+++ 2023/day09-part2/src/index.ts
@@ -0,0 +1,65 @@
+import input from './input';
+
+class OASIS {
+  reading: number[];
+  extrapolations: number[][]
+
+  constructor(reading: string) {
+    this.reading = reading.split(/ +/).map(n => parseInt(n));
+    this.extrapolations = [];
+  }
+
+  extraPolateReading = (reading: number[]) => {
+    const newReading: number[] = [];
+    reading.forEach((n, i) => {
+      if(i + 1 === reading.length) return
+      newReading.push(reading[i+1] - n);
+    })
+    return newReading;
+  }
+                                   
+  extrapolate = () => {
+    let reading = [...this.reading];
+    this.extrapolations = [[...reading]];
+    while(true){
+      reading = [...this.extraPolateReading(reading)];
+      this.extrapolations.push(reading);
+      if (reading.every(n => n === 0)) break;
+    }
+  }
+
+  calculateNextValue = (extrapolations: number[][]) => {
+    extrapolations[extrapolations.length-1].push(0);
+    let calculation = 0;
+    for (let i = extrapolations.length-1; i > 0; i--){
+      const extrapolation = extrapolations[i];
+      const curLastDigit = extrapolation[extrapolation.length-1];
+      const previousRow = extrapolations[i - 1];
+      const prevLastDigit = previousRow[previousRow.length-1];
+      calculation = curLastDigit + prevLastDigit;
+      previousRow.push(calculation);
+    }
+    return calculation;
+  }
+
+  calculatePreviousValue = (extrapolations: number[][]) => {
+    extrapolations[extrapolations.length-1] = [0, ...extrapolations[extrapolations.length-1]]
+    let calculation = 0;
+    for (let i = extrapolations.length-1; i > 0; i--){
+      const extrapolation = extrapolations[i];
+      const curFirstDigit = extrapolation[0];
+      const previousRow = extrapolations[i - 1];
+      const prevFirstDigit = previousRow[0];
+      calculation = prevFirstDigit - curFirstDigit;
+      extrapolations[i - 1] = [calculation, ...extrapolations[i - 1]];
+    }
+    return calculation;
+  }
+}
+
+const sum = input.split(/\n/).map(reading => {
+  const o = new OASIS(reading)
+  o.extrapolate();
+  return o.calculatePreviousValue(o.extrapolations);
+}).reduce((a, b) => a+b)
+console.log(sum)
